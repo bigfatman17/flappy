e2bcbca1c577e9d99d244db76c2f241af5fda624
bigfatman17 <bigfatman17@gmail.com>
2016-12-03 14:19:37 -0500

    Added a bird.

--- flappy.cpp
+++ flappy.cpp
@@ -8,23 +8,38 @@ struct Image
 {
     SDL_Surface* image;
     short x, y;
-    Image(std::string file, short x, short y) { image = SDL_DisplayFormat(IMG_Load(file.c_str()));
+    //centers if no coordinates are given
+    Image(std::string file)                   { image = IMG_Load(file.c_str());
+                                                this->x = WinWidth / 2 - image->w / 2;
+                                                this->y = WinHeight / 2 - image->h / 2; }
+    Image(std::string file, short x, short y) { image = IMG_Load(file.c_str());
                                                 this->x = x;
                                                 this->y = y; }
     ~Image() { SDL_FreeSurface(image); }
 };
 template<typename T, typename... R>
-static void render(SDL_Surface* sdl, T* i, R&&... r)
+static void render(SDL_Surface* sdl, const T* i, R&&... r)
 {
     render(sdl, i);
     render(sdl, std::forward<R>(r)...);
 }
 template<>
-inline void render<Image>(SDL_Surface* sdl, Image* i)
+inline void render<Image>(SDL_Surface* sdl, const Image* i)
 {
     SDL_Rect r{i->x, i->y, 0, 0};
     SDL_BlitSurface(i->image, nullptr, sdl, &r);
 }
+
+template<unsigned acc>
+struct Bird
+{
+    Image* img;
+    unsigned vel{};
+
+    Bird(Image* img) { *this->img = *img; }
+
+    void update(unsigned frame) { if (!(frame % 5)) { vel += acc; img->y += vel; } }
+};
 
 int main()
 {
@@ -32,6 +47,7 @@ int main()
 
     Image bgnd("assets/bg1.png", 0, 0);
     Image bgnd2("assets/bg1.png", 288, 0);
+    Bird<1> bird(new Image("assets/bird2.png"));
 
     bool quit = false;
     unsigned frame{};
@@ -41,12 +57,15 @@ int main()
         if (!(frame % 10)) { bgnd.x--; bgnd2.x--; }
         if (bgnd.x < -288) bgnd.x = 287;
         if (bgnd2.x < -288) bgnd2.x = 287;
+        bird.update(frame);
 
-        render(sdl, &bgnd, &bgnd2);
+        render(sdl, &bgnd, &bgnd2, bird.img);
 
         for (SDL_Event e; SDL_PollEvent(&e); ) {
             if ((e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_ESCAPE) || e.type == SDL_QUIT)
                 quit = true;
+            if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_SPACE)
+                bird.vel = -10;
         }
 
         frame = frame > 60 ? 0 : frame;
