740d27ce13e5dcef7e0242e4c5e8502cfec90c7b
bigfatman17 <bigfatman17@gmail.com>
2016-12-10 14:25:18 -0500

    Implemented rule of five for SDL_Surfaces. Framerate improved; causing a problem. Need to regulate frames again.

--- flappy.cpp
+++ flappy.cpp
@@ -10,18 +10,29 @@ constexpr unsigned WinWidth = 288, WinHeight = 512;
 // Everything is based on 60 frames per second.
 static constexpr unsigned BgndVelocity = 10, BirdAcceleration = 5, PipeVelocity = 2, PipeSpacing = 100, PipeBounds = 200;
 
+class Surface
+{
+    SDL_Surface* s;
+public:
+    SDL_Surface* S() const { return s; }
+    Surface(std::string file) { s = IMG_Load(file.c_str()); }
+    Surface(const Surface& o) { s = SDL_ConvertSurface(o.s, o.s->format, 0); }
+    Surface(Surface&& o)      { swap(*this, o); }
+    friend void swap(Surface& a, Surface& b) { using std::swap; swap(a.s, b.s); }
+    Surface& operator=(Surface o) { swap(*this, o); return *this; }
+    ~Surface() { SDL_FreeSurface(s); }
+};
 struct Image
 {
-    SDL_Surface* image;
+    Surface image;
     short x, y;
+    unsigned short w, h;
     //centers if no coordinates are given
-    Image(std::string file)                   { image = IMG_Load(file.c_str());
-                                                this->x = WinWidth / 2 - image->w / 2;
-                                                this->y = WinHeight / 2 - image->h / 2; }
-    Image(std::string file, short x, short y) { image = IMG_Load(file.c_str());
-                                                this->x = x;
-                                                this->y = y; }
-    ~Image() { SDL_FreeSurface(image); }
+    Image(std::string file) : image(file)                   { w = image.S()->w; h = image.S()->h;
+                                                              x = WinWidth / 2 - w / 2;
+                                                              y = WinWidth / 2 - h / 2; }
+    Image(std::string file, short x, short y) : image(file) { w = image.S()->w; h = image.S()->h;
+                                                              this->x = x; this->y = y; }
 };
 template<typename T, typename... R>
 static void render(SDL_Surface* sdl, const T* i, R&&... r)
@@ -33,7 +44,7 @@ template<>
 inline void render<Image>(SDL_Surface* sdl, const Image* i)
 {
     SDL_Rect r{i->x, i->y, 0, 0};
-    SDL_BlitSurface(i->image, nullptr, sdl, &r);
+    SDL_BlitSurface(i->image.S(), nullptr, sdl, &r);
 }
 
 struct Bird
@@ -49,7 +60,7 @@ struct Bird
 struct Pipe
 {
     Image* up, *down;
-    Pipe(Image* up, Image* down) { this->up = up; this->down = down; this->up->y = WinHeight; this->down->y = -this->down->image->h; }
+    Pipe(Image* up, Image* down) { this->up = up; this->down = down; this->up->y = WinHeight; this->down->y = -this->down->h; }
     void update(unsigned frame)
     {
         if (!(frame % PipeVelocity)) { up->x--; down->x--; }
@@ -58,7 +69,7 @@ struct Pipe
             up->x = WinWidth; down->x = WinWidth;
             unsigned pos = std::rand() % ((WinHeight - PipeBounds) - PipeBounds) + PipeBounds;
             up->y = pos;
-            down->y = pos - PipeSpacing - down->image->h;
+            down->y = pos - PipeSpacing - down->h;
         }
     }
 };
@@ -74,17 +85,17 @@ int main()
     Bird bird(new Image("assets/bird2.png"));
 
     Pipe pipe(new Image("assets/pipeup.png"), new Image("assets/pipedown.png"));
-    pipe.up->x = pipe.up->image->w; pipe.down->x = pipe.down->image->w;
+    pipe.up->x = pipe.up->w; pipe.down->x = pipe.down->w;
     Pipe pipe2(new Image("assets/pipeup.png"), new Image("assets/pipedown.png"));
-    pipe2.up->x = pipe2.up->image->w + WinWidth / 2; pipe2.down->x = pipe2.down->image->w + WinWidth / 2;
+    pipe2.up->x = pipe2.up->w + WinWidth / 2; pipe2.down->x = pipe2.down->w + WinWidth / 2;
     Pipe pipe3(new Image("assets/pipeup.png"), new Image("assets/pipedown.png"));
-    pipe3.up->x = pipe3.up->image->w + WinWidth; pipe3.down->x = pipe3.down->image->w + WinWidth;
+    pipe3.up->x = pipe3.up->w + WinWidth; pipe3.down->x = pipe3.down->w + WinWidth;
 
     //collision checking
     auto coll = [](Bird bird, Pipe pipe)
     {
-        return bird.img->x + bird.img->image->w > pipe.up->x && bird.img->x < pipe.up->x + pipe.up->image->w
-            && (bird.img->y < pipe.down->y + pipe.down->image->h || bird.img->y + bird.img->image->h > pipe.up->y);
+        return bird.img->x + bird.img->w > pipe.up->x && bird.img->x < pipe.up->x + pipe.up->w
+            && (bird.img->y < pipe.down->y + pipe.down->h || bird.img->y + bird.img->h > pipe.up->y);
     };
 
     bool quit{}, lose{};
@@ -95,8 +106,8 @@ int main()
         bird.update(frame);
         if (!lose) {
             if (!(frame % BgndVelocity)) { bgnd.x--; bgnd2.x--; }
-            if (bgnd.x < -bgnd.image->w) bgnd.x = WinWidth - 1;
-            if (bgnd2.x < -bgnd2.image->w) bgnd2.x = WinWidth - 1;
+            if (bgnd.x < -bgnd.w) bgnd.x = WinWidth - 1;
+            if (bgnd2.x < -bgnd2.w) bgnd2.x = WinWidth - 1;
             pipe.update(frame); pipe2.update(frame); pipe3.update(frame);
             lose = coll(bird, pipe) || coll(bird, pipe2) || coll(bird, pipe3);
         }
